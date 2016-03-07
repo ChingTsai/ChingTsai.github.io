ce1533cde937f82ff55ec29627fae29f391c840c
ChingTsai <ctsai@netdb.cs.nthu.edu.tw>
2016-03-07 14:25:02 +0800

    Fix date bug

--- auto_post.sh
+++ auto_post.sh
@@ -9,12 +9,14 @@ parse=`echo $title | sed -e "s/[[:punct:]]//g"`
 parse=`echo $parse | sed -e "s/ /-/g"`
 
 create_date=`date +"%Y-%m-%d"`
+create_date_time=`date +"%Y-%m-%d %H:%M:%S"`
 md_name="_posts/$create_date-$parse.md"
 
 echo "---" > $md_name
 echo "layout : post" >> $md_name
 echo "cover: false" >> $md_name
 echo "title: '$title'" >> $md_name
+echo "date: '$create_date_time'" >> $md_name
 echo "tags: $tags" >> $md_name
 echo "subclass: 'post tag-$tags'" >> $md_name
 echo "categories: 'CT'" >> $md_name
